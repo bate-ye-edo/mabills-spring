6ff80dafbebeecb1fe17e7c0a293f53ddee6199c
WolfYe98 <mingye@hotmail.es>
2024-04-03 18:09:34 +0200

    initial commit

---
+++ src/main/java/es/upm/mabills/MabillsApplication.java
@@ -0,0 +1,13 @@
+package es.upm.mabills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MabillsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MabillsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/upm/mabills/MabillsApplicationTests.java
@@ -0,0 +1,13 @@
+package es.upm.mabills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MabillsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
