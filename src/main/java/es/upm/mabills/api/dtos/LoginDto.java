62ca36633203e58b57a4a9d836457bfee8f0e1a7
WolfYe98 <mingye@hotmail.es>
2024-04-07 01:02:00 +0200

    Solved UserResourceTest errors

--- src/main/java/es/upm/mabills/api/dtos/LoginDto.java
+++ src/main/java/es/upm/mabills/api/dtos/LoginDto.java
@@ -1,11 +1,15 @@
 package es.upm.mabills.api.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginDto {
     @NonNull
     private String username;
