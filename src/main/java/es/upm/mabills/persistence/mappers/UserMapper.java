d493b806575d44e77c5fb0f3192c27c672f932eb
WolfYe98 <mingye@hotmail.es>
2024-04-06 21:53:55 +0200

    Solved MapStruct and Lombok problems during build

--- src/main/java/es/upm/mabills/persistence/mappers/UserMapper.java
+++ src/main/java/es/upm/mabills/persistence/mappers/UserMapper.java
@@ -2,8 +2,9 @@ package es.upm.mabills.persistence.mappers;
 
 import es.upm.mabills.model.User;
 import es.upm.mabills.persistence.entities.UserEntity;
+import org.mapstruct.Mapper;
 
-@SpringMapper
+@Mapper
 public interface UserMapper {
     User toUser(UserEntity userEntity);
 }
