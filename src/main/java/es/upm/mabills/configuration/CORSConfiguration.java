e18fa63d853254bab518a2d9f06c89a89e444ac2
WolfYe98 <mingye@hotmail.es>
2024-04-11 22:19:16 +0200

    Added configuration to prevent CORS

---
+++ src/main/java/es/upm/mabills/configuration/CORSConfiguration.java
@@ -0,0 +1,31 @@
+package es.upm.mabills.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfiguration implements WebMvcConfigurer {
+    private String[] allowedOrigins;
+
+    @Autowired
+    public CORSConfiguration(@Value("${cors.allowed-origins}") String allowedOrigins) {
+        this.setAllowedOrigins(allowedOrigins);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Content-Type", "Authorization")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+
+    private void setAllowedOrigins(String allowedOrigins) {
+        this.allowedOrigins = allowedOrigins.split(",");
+    }
+}
